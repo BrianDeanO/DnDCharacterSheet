7742c85b429c8255c10f3f6638a17ed72a341f04
BrianDeanO <brianrobertdean@gmail.com>
2022-12-14 18:58:32 -0800

    Overhauling the basic SVG template. And creating rough draft for character sheet header

---
+++ dnd/src/characterPage.js
@@ -0,0 +1,13 @@
+import { Component, ReactComponentElement } from "react";
+import React from "react";
+import CharacterInfo from "./MainCharacterSheet/characterInfo";
+
+const CharacterPage = () => {
+
+    return (
+        <CharacterInfo />
+        
+    );
+}
+
+export default CharacterPage;
